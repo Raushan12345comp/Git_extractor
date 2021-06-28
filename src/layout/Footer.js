fbd998cb07145471f4a0eca05a5b0adaf0e84dd7
Raushan Kumar <rk.777raushan@gmail.com>
2021-06-28 18:35:32 +0530

    third-commit

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Container } from 'reactstrap';
+import React from "react";
+import { Container } from "reactstrap";
+
+import { FaFacebook, FaInstagramSquare, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -8,7 +10,18 @@ const Footer = () => {
       tag="footer"
       className="text-center bg-secondary text-white text-uppercase fixed-bottom p-3 mt-2 border-top border-primary"
     >
-        connect with us... 1. facebook 2. instagram 3. twitter
+      <div >
+        Connect with us..
+        <a style={{marginLeft: "50px", fontSize: "20px"}} target="_blank" href="https://www.facebook.com/profile.php?id=100011469290257" >
+          <FaFacebook />
+        </a>
+        <a target="_blank" style={{marginLeft: "50px", fontSize: "20px"}} href="https://www.instagram.com/raushan7406/?hl=en">
+          <FaInstagramSquare />
+        </a>
+        <a target="_blank" style={{marginLeft: "50px", fontSize: "20px"}} href="https://twitter.com/Raushan38854836">
+          <FaTwitter />
+        </a>
+        </div>
     </Container>
   );
 };
